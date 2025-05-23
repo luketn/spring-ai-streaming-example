d7e0d74d4bb2492894e01f5b3d6e8ffa1e1142fd
luketn <endeavour9@gmail.com>
2025-05-23 14:49:22 +1000

    little spring things

--- src/main/java/dev/danvega/streaming/Application.java
+++ src/main/java/dev/danvega/streaming/Application.java
@@ -2,12 +2,14 @@ package dev.danvega.streaming;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
+		System.out.println("Application started" +run.getClass().getSimpleName());
 	}
 
 }
